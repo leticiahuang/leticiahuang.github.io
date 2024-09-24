de14543e91480302ae22733441db8d9f4f7f815a
leticia1232 <leticia.huang@gmail.com>
2024-09-24 18:48:47 +0100

    updating php

--- form.php
+++ form.php
@@ -1,75 +1,15 @@
 <?php
-//from https://formcarry.com/blog/how-to-create-a-simple-html-contact-form/
 if (isset($_POST['email'])) {
-
-    $email_to = "leticia.ytube@gmail.com";
-    $email_subject = "Email from Leticia's Website";
-
-    function problem($error)
-    {
-        echo "Oh looks like there is some problem with your form data: <br><br>";
-        echo $error . "<br><br>";
-        echo "Please fix those to proceed.<br><br>";
-        die();
-    }
-
-    // validation expected data exists
-    if (
-        !isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['comment'])
-    ) {
-        problem('Oh looks like there is some problem with your form data.');
-    }
-
-    $name = $_POST['name']; // required
-    $email = $_POST['email']; // required
-    $comment = $_POST['comment']; // required
-
-    $error_message = "";
-    $email_exp = "/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/";
-
-    if (!preg_match($email_exp, $email)) {
-        $error_message .= 'Email address does not seem valid.<br>';
-    }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-    if (!preg_match($string_exp, $name)) {
-        $error_message .= 'Name does not seem valid.<br>';
-    }
-
-    if (strlen($comment) < 1) {
-        $error_message .= 'Message should not be less than 1 characters<br>';
-    }
-
-    if (strlen($error_message) > 0) {
-        problem($error_message);
-    }
-
-    $email_message = "Form details following:\n\n";
-
-    function clean_string($string)
-    {
-        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-        return str_replace($bad, "", $string);
-    }
-
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($comment) . "\n";
-
-    // create email headers
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $comment = $_POST["comment"];
+    $to = "leticia.ytube@gmail.com";
+    $subject = "Mail from Leticia's website";
     $headers = 'From: ' . $email . "\r\n" .
         'Reply-To: ' . $email . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);
-?>
-
-    <!-- Replace this as your success message -->
-
-    Thanks for your message.
-
-<?php
+    @mail($to, $subject, $comment, $headers);
 }
 ?>
+
+
